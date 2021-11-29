a122a59a73f0a19485f544fbe7ddd75f82eaea0d
AbdiRabbani <abdirabbani59@gmail.com>
2021-11-29 14:54:52 +0700

    initial commit

--- app/src/main/java/com/example/silaris/MainActivity.kt
+++ app/src/main/java/com/example/silaris/MainActivity.kt
@@ -5,26 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.silaris.ui.HomeFragment
 import com.example.silaris.ui.TrolliFragment
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var photoAdapter: ImageAdapter
+    private var dataList = mutableListOf<ImageData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnMoveActivity: ImageView = findViewById(R.id.profile_btn)
-        btnMoveActivity.setOnClickListener(this)
+        recyclerView = findViewById(R.id.recycle_item)
+        recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
+        photoAdapter = ImageAdapter(applicationContext)
+        recyclerView.adapter = photoAdapter
+
+        dataList.add(ImageData("Title", "Desc", R.drawable.img_item1))
+        dataList.add(ImageData("Title", "Desc", R.drawable.img_item2))
+        dataList.add(ImageData("Title", "Desc", R.drawable.img_item3))
+        dataList.add(ImageData("Title", "Desc", R.drawable.img_item4))
+        dataList.add(ImageData("Title", "Desc", R.drawable.img_item5))
+
+        photoAdapter.setDataList(dataList)
     }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.navigation_home -> {
-                val moveIntent = Intent(this@MainActivity, TrolliFragment::class.java)
-                startActivity(moveIntent)
-            }
-        }
-    }
 }
