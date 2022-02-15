67a3d2ac36bd9f1e1d106327210c6966b8d9169d
AbdiRabbani <abdirabbani59@gmail.com>
2022-02-15 14:44:01 +0700

    initial commit

--- app/src/main/java/com/example/silaris/ui/TrolliFragment.kt
+++ app/src/main/java/com/example/silaris/ui/TrolliFragment.kt
@@ -6,55 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.silaris.R
+import com.example.silaris.databinding.FragmentNotifBinding
+import com.example.silaris.databinding.FragmentTrolliBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TrolliFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TrolliFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentTrolliBinding? = null
+    private val binding get() = _binding as FragmentTrolliBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trolli, container, false)
+        _binding = FragmentTrolliBinding.inflate(layoutInflater, container, false)
+        return binding.root
+
+
+//        binding.addBtn.setOnClickListener {
+//            var i = binding.numberTrolli.text.toString()
+//            var a = 0
+//            while (i < i + 1){
+//                a += i
+//                i++
+//                if (a == i + 1){
+//                    break
+//                }
+//
+//
+//            }
+//        }
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TrolliFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TrolliFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addBtn.setOnClickListener {
+            binding.numberTrolli.text = (binding.numberTrolli.text.toString().toInt()+1).toString()
+        }
+        binding.minBtn.setOnClickListener {
+            binding.numberTrolli.text = (binding.numberTrolli.text.toString().toInt()-1).toString()
+        }
     }
+
+
+
 }
